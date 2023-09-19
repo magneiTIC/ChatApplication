aa19104c72f3cebbb0fa791eedf490e84cfc020c
Awa <awa.ndiaye3@uadb.edu.sn>
2023-09-19 21:27:28 +0000

    firebase done by adn

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,10 +12,14 @@ import { HeaderComponent } from './components/fixed/header/header.component'
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule} from '@angular/material/toolbar'
-import{MatIconModule} from '@angular/material/icon';
+import{ MatIconModule } from '@angular/material/icon';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './views/auth/login/login.component';
 import { SignupComponent } from './views/auth/signup/signup.component'
+import { environment } from 'src/environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +33,7 @@ import { SignupComponent } from './views/auth/signup/signup.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
@@ -36,6 +42,8 @@ import { SignupComponent } from './views/auth/signup/signup.component'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   exports:[
     MatFormFieldModule,
---
+++ Frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: Auth) { }
+
+  // Méthode de connexion par e-mail/mot de passe
+  async signIn(email: string, password: string) {
+    try {
+      await signInWithEmailAndPassword(this.auth, email, password);
+      // Connexion réussie, redirigez l'utilisateur ou effectuez d'autres actions.
+    } catch (error) {
+      console.error('Erreur de connexion :', error);
+      // Gérez les erreurs d'authentification ici.
+    }
+  }
+  
+  async signOut() {
+    try {
+      await this.auth.signOut();
+      // Déconnexion réussie, redirigez l'utilisateur ou effectuez d'autres actions.
+    } catch (error) {
+      console.error('Erreur de déconnexion :', error);
+      // Gérez les erreurs de déconnexion ici.
+    }
+  }
+}
