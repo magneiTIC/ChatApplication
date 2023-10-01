b0238a55aca75e2fcf023a6bf0e7776d6a7f1da1
Habibatou Drame <habiibad@gmail.com>
2023-10-01 17:42:36 +0000

    Habiba- socket back end step 002

---
+++ Backend/config/db.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+   
+    mongoURI: 'mongodb://localhost:27017/ChatApplication', 
+
+    mongoOptions: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  };
+  
